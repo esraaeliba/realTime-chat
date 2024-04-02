c1cfea7c593f8a49cf1de6fa4ea7809f5c7b287c
Esraa Ahmed <esraaeliba@gmail.com>
2024-04-02 14:43:37 +0200

    updates in server,js

--- server.js
+++ server.js
@@ -4,11 +4,14 @@ const path = require("path");
 const app = express();
 const server =require("http").createServer(app);
 
-const PORT =process.env.PORT || 5000;
+const PORT =process.env.PORT || 3000;
 const io=require("socket.io")(server);
 
 
 app.use(express.static(path.join(__dirname+"/public")))
+app.get("/", (req, res) =>{
+    res.render("index.ejs");
+});
 
 io.on("connection", function(socket) {
     socket.on("newuser",function(username){
